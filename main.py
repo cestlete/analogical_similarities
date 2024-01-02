dde50299e67532de3ae4ca8e2d73c34cfa7b70df
C'est l'été <hmq897@gmail.com>
2024-01-02 15:50:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
 
 """
 # Welcome to Streamlit!
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+from openie import StanfordOpenIE
+import os
+import csv
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+properties = {
+    'openie.affinity_probability_cap': 2 / 3,
+}
+# https://stanfordnlp.github.io/CoreNLP/openie.html#api
+# Default value of openie.affinity_probability_cap was 1/3.
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+def process_document(txt, doc_name):
+    with StanfordOpenIE(properties=properties) as client:
+        # text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
+        # print('Text: %s.' % txt)
+        # triple_list = client.annotate(txt)
+        heading = [["NOUN", "VERB/PREP", "NOUN"]]
+        with open(data_path + doc_name + ".OIE", 'w+', encoding="utf8") as resultFile:
+            write = csv.writer(resultFile, lineterminator='\n')
+            write.writerows(heading)
+            for triple in client.annotate(txt):
+                write.writerow([triple['subject'], triple['relation'], triple['object']])
+                print(triple)
+        resultFile.close()
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+process_document("Barack Obama was born in Hawaii. Richard Manning wrote this sentence.", "test1.txt")
